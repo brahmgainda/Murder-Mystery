5d85fcf465798d30c7c643fe89273ad196de798e
brahmgainda <brahmjeet.gainda@rbe.sk.ca>
2020-09-23 15:13:25 +0000

    added Pseudocode

---
+++ pseudocode.py
@@ -0,0 +1,19 @@
+
+
+# Start game by giving player backstory
+# define movement variables
+# define interaction variables
+# define useuables  variables
+# display list of suspects and clues
+
+# give player option to investigate a suspects
+# give player option to move one or keep investigating
+# if player investigates more give option to tell the gang he solved the case
+# if player moves one give him option to investigate another suspects
+# Also if  player moves on give option to re investigate the crime scence 
+# if player chooses suspect, give option to move on or say they solved the case
+# if player chooses crime scence, tell player they found a new clue
+# Also if player chooses crime scence give option to keep searching
+
+# player may investigate each suspect once 
+# game is won when player chooses the right person as the murderer
