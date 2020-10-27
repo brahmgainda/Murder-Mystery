317656f93148ea1e36ec48c8a8915323b836112d
brahmgainda <brahmjeet.gainda@rbe.sk.ca>
2020-10-27 03:13:17 +0000

    updated menu formatting

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
-menu = ['A : Check Suspects List', ' B : Investigate Crime Scene', 
-'C : Check Persuasion Points', 'D : Check Time ']
+menu = ['A : Check Suspects List', ' B : Investigate Crime Scene',
+        'C : Check Persuasion Points', 'D : Check Time ']
 
 print("Please Select: ")
 for menus in menu[:4]:
@@ -11,9 +11,10 @@ if selection.lower() == 'a':
 elif selection.lower() == 'b':
     print("You found a new clue at the crime scence its:")
 elif selection.lower() == 'c':
-    print("You need 10 persuasion points agaonst a suspect to prove to the gang they did it, here how many you have:")
+    print("You need 10 persuasion points against a suspect \
+    to prove to the gang they did it, here how many you have:")
 elif selection.lower() == 'd':
-    print("The gang only gave you 24 hrs to solve the case, here how much time you have left:")
+    print("The gang only gave you 24 hrs to solve the case, \
+    here how much time you have left:")
 else:
     print("Unknown Option Selected!")
-    
